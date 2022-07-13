1bd0834bf2d8db9701f79e67a19d7a26308d6edd
Ashraf-Alshashaa <ashraf.alchechae@gmail.com>
2022-07-13 12:44:18 +0200

    Add SearchBar & header & footer

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'use strict';
 
 import {initWelcomePage}  from './pages/WelcomePage.js';
+import {loadSearch}  from './pages/searchPage.js';
 
 const loadApp = () => {
   initWelcomePage ();
 };
 
-window.addEventListener('load', loadApp);
+window.addEventListener('load', () => {
+  loadApp();
+  loadSearch ();
+});
