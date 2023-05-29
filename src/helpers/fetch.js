c4210e6ca08597d47e7ee5ef54f9102c3ea1feb7
ashraf alshashaa <ashraf.alchechae@gmail.com>
2023-05-30 00:04:57 +0200

    delete old files & folders

--- src/helpers/fetch.js
+++
@@ -1,6 +0,0 @@
-export const getJsonData = async () => {
-  const response = await fetch("/phones", {
-    method: "get",
-  });
-  return response.json();
-};
--- src/views/cardView.js
+++
@@ -1,8 +0,0 @@
-export const createCardElement = (phone_name, image_src) => {
-  const element = document.createElement("div");
-  element.innerHTML = String.raw`
-    <img src="${image_src}"/>
-    <h1>${phone_name}</h1>
-    `;
-  return element;
-};
