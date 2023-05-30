431fdb129f2c54a47d2ad10047c416e76ab70db7
ashraf alshashaa <ashraf.alchechae@gmail.com>
2023-05-30 20:46:02 +0200

    move handling errors to controllers functions

--- backend/app/controllers.py
+++ backend/app/controllers.py
@@ -1,14 +1,22 @@
 from app import db
-from flask import request, jsonify
 
 def get_collections_names():
     collection_names = db.list_collection_names()
-    return collection_names
+    try:
+      return {"success": True, "result": collection_names }, 200
+    except Exception as e:
+      return {"success": False, "msg": "Unable to get collections, try again later"}, 500
 
 def get_brand_items(brand_name):
     brand_items = db[brand_name].find()
-    return list(brand_items)
+    try:
+      return {"success": True, "result":list(brand_items)}, 200
+    except Exception as e:
+      return {"success": False, "msg": "Unable to get brand items, try again later"}, 500
 
 def get_mobile(brand_name, id):
     mobile = db[brand_name].find({"_id": id})
-    return list(mobile)
+    try:
+      return {"success": True, "result": list(mobile)[0]}, 200
+    except Exception as e:
+      return {"success": False, "msg": "Unable to get mobile, try again later"}, 500
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,24 +4,15 @@ from app import controllers
 
 @app.route('/api/collections_names', methods=['GET'])
 def collection_names():
-    try:
-      return {"success": True, "result": controllers.get_collections_names()}, 200
-    except Exception as e:
-      return {"success": False, "msg": "Unable to get collections, try again later"}, 500
-
+    return controllers.get_collections_names()
+    
 @app.route('/api/brand/', methods=['GET'])
 def brand_items():
-    try:
-      brand_name = request.args.get("brand")
-      return {"success": True, "result":controllers.get_brand_items(brand_name)}, 200
-    except Exception as e:
-      return {"success": False, "msg": "Unable to get brand items, try again later"}, 500
+    brand_name = request.args.get("brand")
+    return controllers.get_brand_items(brand_name)
     
 @app.route('/api/mobile/', methods=['GET'])
 def mobile():
     brand_name = request.args.get("brand")
     id = request.args.get("id")
-    try:
-      return {"success": True, "result": controllers.get_mobile(brand_name, id)[0]}, 200
-    except Exception as e:
-      return {"success": False, "msg": "Unable to get mobile, try again later"}, 500
+    return controllers.get_mobile(brand_name, id)
