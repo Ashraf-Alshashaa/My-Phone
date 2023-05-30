941ecf3ecb281f2531e764bbf13d932baf6cf501
ashraf alshashaa <ashraf.alchechae@gmail.com>
2023-05-30 16:11:54 +0200

    create get brand route

--- backend/app/controllers.py
+++ backend/app/controllers.py
@@ -1,5 +1,10 @@
 from app import db
+from bson.json_util import dumps
 
 def get_collections_names():
     collection_names = db.list_collection_names()
     return collection_names
+
+def get_brand_items(brand_name):
+    brand_items = db[brand_name].find()
+    return list(brand_items)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,12 @@
-from flask import jsonify
+from flask import request, jsonify
 from app import app
 from app import controllers
 
 @app.route('/api/collections_names', methods=['GET'])
 def collection_names():
-    return {"success": True, "result": controllers.get_collections_names()}
+    return {"success": True, "result": controllers.get_collections_names()}, 200
+
+@app.route('/api/brand/', methods=['GET'])
+def brand_items():
+    brand_name = request.args.get("brand")
+    return {"success": True, "result":controllers.get_brand_items(brand_name)}, 200
