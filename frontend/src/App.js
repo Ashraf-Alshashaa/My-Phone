e1c1e09203d1c439c124c95781af6b4d97ce3af1
ashraf alshashaa <ashraf.alchechae@gmail.com>
2023-06-02 00:55:45 +0200

    Added React Router DOM package for routing functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">home</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div className="App">home</div>}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
