4b701ae1a14e662476e08a593caf442141158d8b
ashraf alshashaa <ashraf.alchechae@gmail.com>
2023-06-04 23:17:57 +0200

    Create Home page files & Add Home page to the route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div className="App">home</div>}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const Home = () => {
+  return <div>hero</div>;
+};
+
+export default Home;
